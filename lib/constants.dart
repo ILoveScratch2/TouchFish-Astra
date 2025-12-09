ccd6506f463eeba57fc5cb1c3cbd36e2d01055b4
ILoveScratch <yaowendao2023@qq.com>
2025-12-09 22:23:01 +0800

    [RELEASE] v2.0.1 (201)

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,15 +1,15 @@
 /// 应用常量 - 单一数据源
 class AppConstants {
   // 版本信息
-  static const String version = '1.0.1';
-  static const String buildNumber = '100';
+  static const String version = '2.0.1';
+  static const String buildNumber = '201';
   static const String appName = 'TouchFishAstra';
   static const String author = 'ILoveScratch2';
 
   // 项目信息
   static const String description =
-      'A modern, cross-platform TouchFish client built with Flutter.\n\nTouchFishAstra is Copyleft free software: you are free to use, study, share, and improve it at any time.';
-  static const String descriptionZh = '基于 Flutter 构建的现代化跨平台 TouchFish 客户端\n\nTouchFishAstra 是 Copyleft 的自由软件:您可以随时使用、研究共享和改进它。';
+      'A modern, cross-platform TouchFish client built with Flutter(supported TouchFish V4 protocol).\n\nTouchFishAstra is Copyleft free software: you are free to use, study, share, and improve it at any time.';
+  static const String descriptionZh = '基于 Flutter 构建的现代化跨平台 TouchFish 客户端（已适配TouchFish V4 协议）\n\nTouchFishAstra 是 TouchFish V4 协议的客户端。\nTouchFishAstra 是 Copyleft 的自由软件:您可以随时使用、研究共享和改进它。';
   static const String githubUrl =
       'https://github.com/ILoveScratch2/TouchFishAstra';
   static const String license = 'Copyright (c) 2025 ILoveScratch2. Licensed under AGPL-3.0 License.';
