2327039dcc5b0b77ec7f907fa157236865c62318
ILoveScratch <yaowendao2023@qq.com>
2025-12-23 13:18:57 +0800

    fix: fix text

--- lib/constants.dart
+++ lib/constants.dart
@@ -9,7 +9,7 @@ class AppConstants {
   // 项目信息
   static const String description =
       'A modern, cross-platform TouchFish client built with Flutter(supported TouchFish V4 protocol).\n\nTouchFishAstra is Copyleft free software: you are free to use, study, share, and improve it at any time.';
-  static const String descriptionZh = '基于 Flutter 构建的现代化跨平台 TouchFish 客户端（已适配TouchFish V4 协议）\n\nTouchFishAstra 是 TouchFish V4 协议的客户端。\nTouchFishAstra 是 Copyleft 的自由软件:您可以随时使用、研究共享和改进它。';
+  static const String descriptionZh = '基于 Flutter 构建的现代化跨平台 TouchFish 客户端（已适配TouchFish V4 协议）\n\nTouchFishAstra 是 Copyleft 的自由软件:您可以随时使用、研究共享和改进它。';
   static const String githubUrl =
       'https://github.com/ILoveScratch2/TouchFishAstra';
   static const String license = 'Copyright (c) 2025 ILoveScratch2. Licensed under AGPL-3.0 License.';
